2a0797e39f0df5cd0d4a21b2f1d1915c425598e9
Simon Garrelou <simon.s.garrelou@airbus.com>
2018-11-30 11:15:00 +0100

    Change the way samples are downloaded

--- event.go
+++ event.go
@@ -51,51 +51,98 @@ type DownloadResponseFile struct {
 	EventInfo   string `json:"event_info"`
 }
 
-// DownloadSample downloads a malware sample to a given file. If hash is empty, download the first available sample for the event
-func (event *Event) DownloadSample(hash string, filename string) error {
-	type requestAllSamples struct {
-		EventID    int `json:"eventID"`
-		AllSamples int `json:"allSamples"`
-	}
-
-	type requestNotAllSamples struct {
-		Hash    string `json:"hash"`
-		EventID int    `json:"eventID"`
-	}
-
-	var actualRequest interface{}
-	eventID, _ := strconv.Atoi(event.ID)
-	if hash == "" {
-		actualRequest = requestAllSamples{
-			EventID:    eventID,
-			AllSamples: 1,
-		}
-	} else {
-		actualRequest = requestNotAllSamples{
-			Hash:    hash,
-			EventID: eventID,
-		}
-	}
-
+func (event *Event) downloadSampleRequest(request interface{}) (*DownloadResponse, error) {
 	resp, err := event.client.Get("/attributes/downloadSample/", Request{
-		Request: actualRequest,
+		Request: request,
 	})
 
 	// Parse response
 	var downloadResponse DownloadResponse
 	jsonDecoder := json.NewDecoder(resp.Body)
 	if err = jsonDecoder.Decode(&downloadResponse); err != nil {
-		return fmt.Errorf("Error decoding response: %s", err)
+		return nil, fmt.Errorf("Error decoding response: %s", err)
 	}
 
 	if len(downloadResponse.Result) == 0 {
-		return fmt.Errorf("No results")
+		return nil, fmt.Errorf("No results")
+	}
+
+	return &downloadResponse, nil
+}
+
+// DownloadSampleByHash downloads a malware sample identified by a hash to a given file.
+func (event *Event) DownloadSampleByHash(hash string, filename string) error {
+	type requestNotAllSamples struct {
+		Hash    string `json:"hash"`
+		EventID int    `json:"eventID"`
+	}
+
+	eventID, _ := strconv.Atoi(event.ID)
+	response, err := event.downloadSampleRequest(requestNotAllSamples{
+		Hash:    hash,
+		EventID: eventID,
+	})
+	if err != nil {
+		return err
 	}
 
 	// Download attachment
 	// TODO: it's cool to use DownloadAttachment because DRY, but we end up downloading the file twice...
-	attrID, _ := strconv.ParseInt(downloadResponse.Result[0].AttributeID, 10, 32)
+	attrID, _ := strconv.ParseInt(response.Result[0].AttributeID, 10, 32)
 	return event.client.DownloadAttachment(int(attrID), filename)
+}
+
+// DownloadNthSample downloads the "n"th sample from this event to the given filename. Starts at 0
+func (event *Event) DownloadNthSample(n int, filename string) error {
+	type requestAllSamples struct {
+		EventID    int `json:"eventID"`
+		AllSamples int `json:"allSamples"`
+	}
+
+	eventID, _ := strconv.ParseInt(event.ID, 10, 32)
+
+	response, err := event.downloadSampleRequest(requestAllSamples{
+		EventID:    int(eventID),
+		AllSamples: 1,
+	})
+	if err != nil {
+		return err
+	}
+
+	if len(response.Result) <= n {
+		return fmt.Errorf("Too few results: %d", len(response.Result))
+	}
+
+	attrID, _ := strconv.ParseInt(response.Result[n].AttributeID, 10, 32)
+	return event.client.DownloadAttachment(int(attrID), filename)
+}
+
+// DownloadAllSamples downloads all samples from the event. filenamePattern should have a %d that will be replaced by the sample index
+func (event *Event) DownloadAllSamples(filenamePattern string) error {
+	type requestAllSamples struct {
+		EventID    int `json:"eventID"`
+		AllSamples int `json:"allSamples"`
+	}
+
+	eventID, _ := strconv.ParseInt(event.ID, 10, 32)
+
+	response, err := event.downloadSampleRequest(requestAllSamples{
+		EventID:    int(eventID),
+		AllSamples: 1,
+	})
+	if err != nil {
+		return err
+	}
+
+	for n, result := range response.Result {
+		attrID, _ := strconv.ParseInt(result.AttributeID, 10, 32)
+		err = event.client.DownloadAttachment(int(attrID), fmt.Sprintf(filenamePattern, n))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // AddTag adds a tag to a given event
